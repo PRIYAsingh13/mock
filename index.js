9eea405534c6970a36b075840a275cdfd1075f2f
PRIYAsingh13 <ps597934@gmail.com>
2023-01-16 16:26:06 +0530

    mi

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+// var a = 1;
+
+// function foo(){
+//     console.log(a);
+//     var a = 2;
+// }
+
+// foo()
+
+
+// function test(){
+//     let a = 20;
+//     return function inner(){
+//         return a;
+//     }
+// }
+// let inner = test();
+// console.log(inner);
+
+function abc (){
+    console.log(this)
+}
+abc()
